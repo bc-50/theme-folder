59a0812a5d4dd53883390f3a4918ad3a2b6c3ee1
Brace Creative <hello@brace.co.uk>
2019-08-19 09:12:17 +0100

    Theme Folder

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+<?php endwhile; else : ?>
+    <h1><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></h1>
+  <?php get_template_part('template-parts/content', 'end'); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function () {
+  var $hamburger = $('.hamburger');
+  $hamburger.on('click', function (e) {
+    $hamburger.toggleClass('is-active');
+    // Do something else, like open/close menu
+  });
+
+});
